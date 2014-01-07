a879d9d08eadbe14bb5d21575d11a49741da7646
labrador <labrador@labradoodle.com>
2014-01-06 18:08:57 -0600

    added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
+
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    // Define the configuration for all the tasks
+    grunt.initConfig({
+
+        parallel:{
+            selenium:{
+                options:{
+                    stream:true
+                },
+                tasks: [{cmd: './bin/selenium-server.sh'}, {cmd: './bin/test.sh'}]
+            }
+        }
+    });
+
+
+    grunt.registerTask('test', [
+        'parallel:selenium'
+    ]);
+
+    grunt.registerTask('default', [
+        'test',
+    ]);
+};
+
--- protractor.conf.js
+++ protractor.conf.js
@@ -11,7 +11,7 @@ exports.config = {
 
     // Spec patterns are relative to the current working directly when
     // protractor is called.
-    specs: ['example_spec.js'],
+    specs: ['specs/test-sample.js'],
 
     // Options to be passed to Jasmine-node.
     jasmineNodeOpts: {
